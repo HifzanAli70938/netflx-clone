636b0583ac040c40d3d75f36a581f3a3bb0680e3
Hifzan Ali <74439970+HifzanAli70938@users.noreply.github.com>
2021-06-12 19:15:07 +0500

    my netflix website

---
+++ src/requests.js
@@ -0,0 +1,16 @@
+
+const APIKEY = "d072e2ded577c770fc22d441add6acc9";
+
+
+const requests = {
+    fetchTreding: `/trending/all/week?api_key=${APIKEY}&language=en-US`,
+    fetchNetFlixOriginals:`/discover/tv?api_key=${APIKEY}&with_networks=213`,
+    fetchTopRated:`/movie/top_rated?api_key=${APIKEY}&language=en-US`,
+    fetchActionMovies:`/discover/movie?api_key=${APIKEY}&with_genres=28`,
+    fetchComedyMovies:`/discover/movie?api_key=${APIKEY}&with_genres=35`,
+    fetchHorroMovies:`/discover/movie?api_key=${APIKEY}&with_genres=27`,
+    fetchRomanticMovies:`/discover/movie?api_key=${APIKEY}&with_genres=10749`,
+    fetchDocumentaries:`/discover/movie?api_key=${APIKEY}&with_genres=99`
+}
+
+export default requests;
